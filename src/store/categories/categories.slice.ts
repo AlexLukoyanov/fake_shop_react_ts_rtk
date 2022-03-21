5d1565cd5abb9ab8d08724f384cd0ea67fb93e69
Alex Lukoyanov <lukoav@gmail.com>
2022-03-21 15:46:19 +0300

    feat: added method to set active category for category state

--- src/store/categories/categories.slice.ts
+++ src/store/categories/categories.slice.ts
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { CategoriesName } from "./categories.type";
 
 const initialState = CategoriesName.All;
@@ -6,7 +6,11 @@ const initialState = CategoriesName.All;
 export const categoriesSlice = createSlice({
   name: "category",
   initialState,
-  reducers: {},
+  reducers: {
+    setActiveCategory: (_, action: PayloadAction<CategoriesName>) =>
+      action.payload,
+  },
 });
 
+export const { setActiveCategory } = categoriesSlice.actions;
 export default categoriesSlice.reducer;
