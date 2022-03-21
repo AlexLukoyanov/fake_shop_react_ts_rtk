616a5eaba545b24e90fec329cd9bf3f90d87705a
Alex Lukoyanov <lukoav@gmail.com>
2022-03-21 15:02:44 +0300

    feat: added get products from server

--- src/store/products/products.slice.ts
+++ src/store/products/products.slice.ts
@@ -1,5 +1,18 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 import { IProduct } from "./products.type";
+
+export const fetchProducts = createAsyncThunk(
+  "products/fetchProducts",
+  async (limit: number, thunkAPI) => {
+    try {
+      const responce = await axios.get<IProduct[]>(
+        "https://fakestoreapi.com/products",
+        { params: { limit: limit } }
+      );
+    } catch (e) {}
+  }
+);
 
 interface ProductsState {
   products: IProduct[];
@@ -19,3 +32,5 @@ export const productsSlice = createSlice({
   reducers: {},
   extraReducers: {},
 });
+
+export default productsSlice.reducer;
