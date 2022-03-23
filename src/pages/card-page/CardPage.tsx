ad566ff895dac473b1eebe67ddb7d2e04360fcef
Alex Lukoyanov <lukoav@gmail.com>
2022-03-23 14:54:04 +0300

    feat: added card page component

--- src/pages/card-page/CardPage.tsx
+++ src/pages/card-page/CardPage.tsx
@@ -1,5 +1,43 @@
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "./../../hooks/redux";
+import { fetchProduct } from "./../../store/products/product.slice";
+import Loader from "./../../components/ui/loader/Loader";
+import styles from "./CardPage.module.scss";
+
 const CardPage = () => {
-  return <div>Enter</div>;
+  const { id } = useParams();
+  const number = Number(id);
+  const dispatch = useAppDispatch();
+  const { product, isLoading } = useAppSelector((state) => state.productSlice);
+
+  useEffect(() => {
+    dispatch(fetchProduct(number));
+  }, []);
+  return (
+    <div className="page">
+      {!isLoading ? (
+        <Loader />
+      ) : (
+        <div className={styles.card_wrapper}>
+          <div className={styles.card_img}>
+            <img src={product.image} alt="product card" />
+          </div>
+          <div className={styles.card_description}>
+            <h3>{product.category}</h3>
+            <h1>{product.title}</h1>
+
+            <h4> $ {product.price}</h4>
+            <p>{product.description}</p>
+            <div>
+              <button>Add to Cart</button>
+              <button>Go to Cart</button>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default CardPage;
--- src/pages/home-page/card-list/card-item/CardItem.tsx
+++ src/pages/home-page/card-list/card-item/CardItem.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react";
+import { Link } from "react-router-dom";
 import { IProduct } from "./../../../../store/products/products.type";
 import styles from "./CardItem.module.scss";
 
@@ -9,7 +10,14 @@ type ItemPtops = {
 const CardItem: FC<ItemPtops> = ({ item }) => {
   return (
     <li className={styles.card_item}>
-      <img src={item.image} width={"80%"} height={"200px"} alt="product card" />
+      <Link to={`/card/${item.id}`}>
+        <img
+          src={item.image}
+          width={"80%"}
+          height={"200px"}
+          alt="product card"
+        />
+      </Link>
 
       <h5>{item.title.substring(0, 15)}...</h5>
 
