8d253461108cab1de197573dd12a92c5ac44b066
Alex Lukoyanov <lukoav@gmail.com>
2022-04-02 09:05:34 +0300

    feat: added firebase config

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCsqTRV3whTrpYyMNOpu7JdvfT1uUN7XbU",
+  authDomain: "ifake-53970.firebaseapp.com",
+  projectId: "ifake-53970",
+  storageBucket: "ifake-53970.appspot.com",
+  messagingSenderId: "413784939676",
+  appId: "1:413784939676:web:a7f23f33612e925b7b2159",
 };
 
 const app = initializeApp(firebaseConfig);
